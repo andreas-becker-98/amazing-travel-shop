c5e44abecfa8bf2710bdb1993fe582ebd83c051b
May Guan <maymayy08@users.noreply.github.com>
2025-03-18 19:58:12 +0000

    Add carousel automatic

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,52 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Homepage = () => {
+  const [currentSlide, setCurrentSlide] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prev) => (prev === 4 ? 1 : prev + 1)); 
+    }, 3000);
+
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <div>
       <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
+        <div id="slide1" className={`carousel-item relative w-full ${currentSlide === 1 ? "block" : "hidden"}`}>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
-            className="w-full"
+            src="https://images.unsplash.com/photo-1634718756869-0350d6bec047?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            className="w-full h-[300px]"
             alt="Slide 1"
           />
-          <div className="absolute left-5 right-5 top-1/2 transform -translate-y-1/2 flex justify-between">
-            <a href="#slide4" className="btn btn-circle">❮</a>
-            <a href="#slide2" className="btn btn-circle">❯</a>
-          </div>
         </div>
-        <div id="slide2" className="carousel-item relative w-full">
+        <div id="slide2" className={`carousel-item relative w-full ${currentSlide === 2 ? "block" : "hidden"}`}>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
-            className="w-full"
+            src="https://thumbs.dreamstime.com/b/hikers-couple-hiking-mountains-canyon-banner-hikers-couple-hiking-mountains-landscape-banner-panorama-woman-man-walking-193401766.jpg"
+            className="w-full h-[300px]"
             alt="Slide 2"
           />
-          <div className="absolute left-5 right-5 top-1/2 transform -translate-y-1/2 flex justify-between">
-            <a href="#slide1" className="btn btn-circle">❮</a>
-            <a href="#slide3" className="btn btn-circle">❯</a>
-          </div>
         </div>
-        <div id="slide3" className="carousel-item relative w-full">
+        <div id="slide3" className={`carousel-item relative w-full ${currentSlide === 3 ? "block" : "hidden"}`}>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
-            className="w-full"
+            src="https://www.shutterstock.com/image-photo/banner-women-camping-outdoor-sitting-600nw-2470907231.jpg"
+            className="w-full h-[300px]"
             alt="Slide 3"
           />
-          <div className="absolute left-5 right-5 top-1/2 transform -translate-y-1/2 flex justify-between">
-            <a href="#slide2" className="btn btn-circle">❮</a>
-            <a href="#slide4" className="btn btn-circle">❯</a>
-          </div>
         </div>
-        <div id="slide4" className="carousel-item relative w-full">
+        <div id="slide4" className={`carousel-item relative w-full ${currentSlide === 4 ? "block" : "hidden"}`}>
           <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
-            className="w-full"
+            src="https://www.shutterstock.com/image-photo/ski-winter-season-mountains-touring-260nw-749876944.jpg"
+            className="w-full h-[300px]"
             alt="Slide 4"
           />
-          <div className="absolute left-5 right-5 top-1/2 transform -translate-y-1/2 flex justify-between">
-            <a href="#slide3" className="btn btn-circle">❮</a>
-            <a href="#slide1" className="btn btn-circle">❯</a>
-          </div>
         </div>
       </div>
     </div>
