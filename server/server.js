a35d6094ed1de55eaab77ec0efe94e4ddb7ff389
Andreas Becker <andreas-becker-98@users.noreply.github.com>
2025-03-18 23:11:12 +0000

    added basic server startup routine

--- server/server.js
+++ server/server.js
@@ -1,1 +1,15 @@
-console.log('This is not working yet!');
+// Require modules & functions
+const express = require('express');
+const sequelize = require('./config/connection');
+
+// Configuration Data
+const PORT = process.env.PORT || 3001;
+
+// Set up server
+const app = express();
+app.use(express.json());
+
+// Synchronise database and start server on success.
+sequelize.sync({ force: false }).then(async () => {
+    app.listen(PORT);
+});
