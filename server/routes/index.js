aae0a15c30f7dc718b9bca23c39e7beebfea94cc
Andreas Becker <andreas-becker-98@users.noreply.github.com>
2025-03-18 23:12:43 +0000

    added authentication route (allows users to log in)

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,38 @@
 const router = require("express").Router();
 
+const User = require('../models/user');
+const { checkPassword, signToken } = require('../utils/auth');
+
+
 router.get("/api", (_, res) => {
     res.json({ message: "Welcome to the API" });
+});
+
+router.get("/api/auth", async (req, res) => {
+    if(!req.body.email || !req.body.password) {
+        res.sendStatus(400);
+        return;
+    }
+
+    const userData = await User.findOne({
+        attributes: ['id', 'email', 'name', 'address', 'password'],
+        where: {
+            email: req.body.email,
+        }
+    });
+
+    if(!userData) {
+        // user does not exist
+        res.statusCode(400);
+        return;
+    }
+
+    if(!checkPassword(req.body.password, userData.dataValues.password)) {
+        res.statusCode(400);
+        return;
+    }
+
+    res.json({token: signToken(userData.dataValues) });
 });
 
 module.exports = router;
