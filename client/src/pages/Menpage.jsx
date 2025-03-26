76145c75e2222e25b014a5aff358fac130cbaf2a
Andreas Becker <andreas-becker-98@users.noreply.github.com>
2025-03-26 23:52:54 +0000

    Frontend: Changed Men's Page to fetch products from the server.

--- client/src/pages/Menpage.jsx
+++ client/src/pages/Menpage.jsx
@@ -1,24 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../contexts/CartContext";
-import { menProducts } from "../data/MenProducts"; 
+import api from '../api';
+
+
 function MenPage() {
+  const [products, setProducts] = useState([]);
   const { addToCart } = useCart();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await api.get(`/api/products/for/m`);
+        console.log(response);
+        setProducts(response.data);
+      } catch {
+        console.error(`Failed to fetch product with type "M"`);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
     <div>
       <div className="product-container">
-        {menProducts.map((product) => (
+        {products.map((product) => (
           <div key={product.id} className="product-card">
-            <h2>{product.name}</h2>
+            <h2>{product.details[0].name}</h2>
             <Link to={`/product/men/${product.id}`}>
               <img
-                src={product.imageUrl}
-                alt={product.name}
+                src={product.image_url}
+                alt={product.details[0].name}
                 className="product-image"
               />
             </Link>
-            <p>Price: £{product.price}</p>
+            <p>Price: £{product.price["gbp"]}</p>
             <button onClick={() => addToCart(product)}>Add to Cart</button>
           </div>
         ))}
