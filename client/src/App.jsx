f5bfaadd9d1ad72f3fe5ae47223ffb985fb59020
Andreas Becker <andreas-becker-98@users.noreply.github.com>
2025-03-27 00:12:11 +0000

    Frontend: Refactored Men's and Women's wear into single component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,8 +10,6 @@ import Cart from "./pages/Cart";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Homepage from "./pages/Homepage";
-import Menpage from "./pages/Menpage";
-import Womenpage from "./pages/Womenpage";
 import Backpacks from "./categories/Backpacks";
 import Jackets from "./categories/Jackets"; 
 import TrailVests from "./categories/TrailVests";
@@ -20,6 +18,7 @@ import SignUp from "./pages/Signup";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./App.css";
 import { SessionProvider } from "./contexts/SessionContext";
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
@@ -30,8 +29,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/product/:category/:id" element={<Product />} />
-          <Route path="/menpage" element={<Menpage />} /> 
-          <Route path="/womenpage" element={<Womenpage />} />
+          <Route path="/menpage" element={<ProductList type="m" longType="men"/>} /> 
+          <Route path="/womenpage" element={<ProductList type="w" longType="women"/>} />
           <Route path="/myaccount" element={<MyAccount />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/cart" element={<Cart />} />
